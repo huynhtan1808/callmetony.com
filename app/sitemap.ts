328acacb9ff1c8795b57e2ac83369d599063a650
huynhtan1808 <tanhuynh1808@gmail.com>
2023-04-20 16:01:56 +0700

    Update Sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ export default async function sitemap() {
     lastModified: post.publishedAt,
   }));
 
-  const routes = ['', '/about', '/blog', '/guestbook', '/uses'].map(
+  const routes = ['', '/about', '/blog', '/workspace'].map(
     (route) => ({
       url: `https://callmetony.com${route}`,
       lastModified: new Date().toISOString().split('T')[0],
